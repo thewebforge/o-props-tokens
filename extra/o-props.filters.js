66a209c85d9228b97ec26a822e2cdf16c1a41e2e
Cédric Bontems <67456035+cbontems@users.noreply.github.com>
2023-12-08 01:20:21 +0100

    feat(gradients): split gradients noises ans filters into their own files for better granularity

---
+++ extra/o-props.filters.js
@@ -0,0 +1,7 @@
+export default {
+  "--filter-1": "contrast(300%) brightness(100%)",
+  "--filter-2": "contrast(200%) brightness(150%)",
+  "--filter-3": "contrast(200%) brightness(250%)",
+  "--filter-4": "contrast(200%) brightness(500%)",
+  "--filter-5": "contrast(200%) brightness(1000%)",
+};
--- extra/o-props.gradients.js
+++ extra/o-props.gradients.js
@@ -1,37 +1,4 @@
 export default {
-  "--color-space": "",
-  "--brand-gradient": `
-linear-gradient(
-  65deg var(--color-space),
-  hsl(var(--brand-hsl) / 80%),
-  hsl(var(--black-hsl) / 0%) 70.71%
-),
-linear-gradient(
-  217deg var(--color-space),
-  hsl(var(--accent-hsl) / 80%),
-  hsl(var(--black-hsl) / 0%) 70.71%
-),
-linear-gradient(
-  135deg var(--color-space),
-  hsl(var(--links-hsl) / 80%),
-  hsl(var(--black-hsl) / 0%) 70.71%
-),
-linear-gradient(
-  270deg var(--color-space),
-  hsl(var(--visited-hsl) / 80%),
-  hsl(var(--black-hsl) / 0%) 70.71%
-)`,
-  "--brand-gradient-stops":
-    "var(--brand), var(--accent), var(--links), var(--visited), var(--brand)",
-  "--brand-gradient-tb":
-    "linear-gradient(to bottom var(--color-space), var(--brand-gradient-stops))",
-  "--brand-gradient-tt":
-    "linear-gradient(to top var(--color-space), var(--brand-gradient-stops))",
-  "--brand-gradient-tr":
-    "linear-gradient(to right var(--color-space), var(--brand-gradient-stops))",
-  "--brand-gradient-tl":
-    "linear-gradient(to left var(--color-space), var(--brand-gradient-stops))",
-
   "--gradient-1":
     "linear-gradient(to bottom right var(--color-space),#1f005c, #5b0060, #870160, #ac255e, #ca485c, #e16b5c, #f39060, #ffb56b)",
   "--gradient-2":
@@ -118,16 +85,4 @@ linear-gradient(
           circle at bottom left var(--color-space), 
           hsl(150 100% 84%), hsl(150 100% 84% / 0%)
         )`,
-
-  "--noise-1": `url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.005' numOctaves='2' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E")`,
-  "--noise-2": `url("data:image/svg+xml,%3Csvg viewBox='0 0 300 300' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.05' numOctaves='1' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E")`,
-  "--noise-3": `url("data:image/svg+xml,%3Csvg viewBox='0 0 1024 1024' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.25' numOctaves='1' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E")`,
-  "--noise-4": `url("data:image/svg+xml,%3Csvg viewBox='0 0 2056 2056' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.5' numOctaves='1' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E")`,
-  "--noise-5": `url("data:image/svg+xml,%3Csvg viewBox='0 0 2056 2056' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.75' numOctaves='1' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E")`,
-
-  "--noise-filter-1": "contrast(300%) brightness(100%)",
-  "--noise-filter-2": "contrast(200%) brightness(150%)",
-  "--noise-filter-3": "contrast(200%) brightness(250%)",
-  "--noise-filter-4": "contrast(200%) brightness(500%)",
-  "--noise-filter-5": "contrast(200%) brightness(1000%)",
 };
